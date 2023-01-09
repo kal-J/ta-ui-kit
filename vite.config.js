32231af8d5d80808c6ba96aa90250ed30fff8b15
kalujja <eric.kalujja@trueafrican.com>
2023-01-10 01:36:53 +0300

    WIP: fixing multple entry points exports

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,13 +11,14 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points
       //entry: resolve(__dirname, 'src/lib/index.js'),
       entry: {
-        vue: resolve(__dirname, 'src/lib/vue/index.js'),
-        react: resolve(__dirname, 'src/lib/react/index.js'),
+        'ta-ui-kit': resolve(__dirname, 'src/lib/index.js'),
+        'ta-ui-kit-vue': resolve(__dirname, 'src/lib/vue/index.js'),
+        'ta-ui-kit-react': resolve(__dirname, 'src/lib/react/index.js'),
       },
-      //formats: ["es", "cjs"],
-      name: 'ta-ui-kit',
+      formats: ["es", "cjs"],
+      //name: 'ta-ui-kit',
       // the proper extensions will be added
-      fileName: 'ta-ui-kit',
+      //fileName: 'ta-ui-kit',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
