2f4447627be08be28adaa4332a61f7285ff459c9
kalujja <eric.kalujja@trueafrican.com>
2023-01-10 02:14:09 +0300

    fix exports

--- src/lib/vue/index.js
+++ src/lib/vue/index.js
@@ -2,3 +2,4 @@ import { applyPureReactInVue } from "veaury";
 import SideBar from "../components/SideBar";
 
 export const SideBar = applyPureReactInVue(SideBar);
+export const SideBar2 = applyPureReactInVue(SideBar);
